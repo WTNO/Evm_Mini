17ee2f74e83af3dc106111be9f56c0cac4a420c4
zhongjiaqi <920093991@qq.com>
2023-09-27 15:41:38 +0800

    memory Resize函数

---
+++ src/memory.js
@@ -0,0 +1,34 @@
+const INIT_SIZE = 8192;
+
+function newSize(value) {
+    const remainder = value % 32;
+    if (r == 0) {
+        return value;
+    } else {
+        return value + 32 - r;
+    }
+}
+
+// 1KB
+const CONTAINER_SIZE = 8192
+
+// 内存地址是以字节为单位，因此使用Uint8Array存储
+class Memory {
+    constructor() {
+        this._store = new Uint8Array(INIT_SIZE);
+    }
+
+    resize(offset, size) {
+        if (size == 0) {
+            return;
+        }
+
+        const newSize = newSize(offset + size);
+        // 所需大小大于当前大小才扩容
+        const diff = newSize - this._store.length;
+        if (diff > 0) {
+            const  expandSize = Math.ceil(diff / CONTAINER_SIZE) * CONTAINER_SIZE;
+            // TODO:扩容数组函数
+        }
+    }
+}
