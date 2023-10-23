38acf0d8f1ce1aeed1567b60c24c80e0ea669ed4
zhongjiaqi <920093991@qq.com>
2023-10-23 15:00:55 +0800

    Interpreter (在看函数放在Map中要怎么使用)

--- src/Interpreter.js
+++ src/Interpreter.js
@@ -1,5 +1,33 @@
-//  解释器
+import { Memory } from "./memory";
+import { Stack } from "./stack";
 
-function run(bytecode) {
+//  解释器
+export class Interpreter {
+    constructor(hex) {
+        this.bytecode = hexToBytes(data);
+        this.context = {
+            programCounter: 0,
+            memory: new Memory(),
+            stack: new Stack(),
+
+        }
+    }
+
+    getCodeSize() {
+        return this.bytecode.length;
+    }
+
+    getCode() {
+        return this.bytecode;
+    }
     
+    getCallValue() {
+        return 0;
+    }
+
+    run() {
+        while (this.programCounter < this.bytecode.length) {
+            
+        }
+    }
 }
