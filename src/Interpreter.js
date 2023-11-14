b9efdfdfd6d8d45217d048fc8b3ac441afcafcee
zhongjiaqi <920093991@qq.com>
2023-11-14 17:45:40 +0800

    修改代码

--- src/Interpreter.js
+++ src/Interpreter.js
@@ -105,20 +105,42 @@ export class Interpreter {
         return Date.now();
     }
 
+    // 当用户A通过合约B来call合约C的时候，执行的是合约C的函数，
+    // 语境(Context，可以理解为包含变量和状态的环境)也是合约C的：msg.sender是B的地址，并且如果函数改变一些状态变量，产生的效果会作用于合约C的变量上。
     call(value, calldata, address) {
-
+        let tx = {
+            nonce: 1,
+            from: this.context.to,
+            to: address,
+            data: bytesToHex(calldata),
+            value: value
+        }
     }
 
+    // 用户A通过合约B来delegatecall合约C的时候，执行的是合约C的函数，
+    // 但是语境仍是合约B的：msg.sender是A的地址，并且如果函数改变一些状态变量，产生的效果会作用于合约B的变量上。
     delegateCall(value, calldata, address) {
-
+        let tx = {
+            nonce: 1,
+            from: this.context.from,
+            to: address,
+            data: bytesToHex(calldata),
+            value: value
+        }
     }
 
     staticCall(value, calldata, address) {
-
+        let tx = {
+            nonce: 1,
+            from: this.context.to,
+            to: address,
+            data: bytesToHex(calldata),
+            value: value
+        }
     }
 
     _call() {
-        
+
     }
 
     run() {
